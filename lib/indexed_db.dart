0a5e721c5ccc888c21db719b67980e01483f4260
Steve Hamblett <steve.hamblett@linux.com>
2024-12-17 10:38:06 +0000

    Initial commit

---
+++ lib/indexed_db.dart
@@ -0,0 +1,17 @@
+/*
+ * Package : Sporran
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 05/02/2014
+ * Copyright :  S.Hamblett@OSCF
+ */
+
+library;
+
+import 'dart:async';
+import 'dart:js_interop';
+import 'dart:typed_data';
+
+import 'package:web/web.dart';
+
+part 'src/indexed_db.dart';
+
