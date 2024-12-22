55f63cabbd38d0f92eb7906e0ecee8f5654450df
Steve Hamblett <steve.hamblett@linux.com>
2024-12-22 09:29:25 +0000

    Main functionality

--- lib/src/indexed_db.dart
+++ lib/src/indexed_db.dart
@@ -20,11 +20,54 @@
 /// The standard does not specify size limits for individual data items
 /// or for the database itself, but browsers may impose storage limits.
 ///
+/// ## Using indexed_db
+///
 /// The classes in this library provide an interface
-/// to the web package IndexedDB implementation.
+/// to the browser's IndexedDB, if it has one.
+/// To use this library in your code:
+///
+///     import 'package:indexed_db/indexed_db';
+///
+/// IndexedDB is almost universally supported in modern web browsers, but
+/// a web app can determine if the browser supports IndexedDB
+/// with [IdbFactory.supported]:
+///
+///     if (IdbFactory.supported)
+///       // Use indexeddb.
+///     else
+///       // Find an alternative.
+///
+/// Access to the browser's IndexedDB is provided by the app's top-level
+/// [Window] object, which your code can refer to with `window.indexedDB`.
+/// So, for example,
+/// here's how to use window.indexedDB to open a database:
+///
+///     Future open() {
+///       return window.indexedDB.open('myIndexedDB',
+///           version: 1,
+///           onUpgradeNeeded: _initializeDatabase)
+///         .then(_loadFromDB);
+///     }
+///     void _initializeDatabase(VersionChangeEvent e) {
+///       ...
+///     }
+///     Future _loadFromDB(Database db) {
+///       ...
+///     }
 ///
 /// All data in an IndexedDB is stored within an [ObjectStore].
 /// To manipulate the database use [Transaction]s.
+///
+/// ## Other resources
+///
+/// Other options for client-side data storage include:
+///
+/// * [Window.localStorage]&mdash;a
+/// basic mechanism that stores data as a [Map],
+/// and where both the keys and the values are strings.
+///
+/// MDN provides [API
+/// documentation](https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API).
 ///
 library;
 
@@ -64,7 +107,7 @@ extension type idbFactory._(IDBFactory factory) {
         request = factory.open(name);
       }
       request.onsuccess = ((Event _) {
-        completer.complete(Database.fromOpenRequest(request.result!));
+        completer.complete(Database._fromOpenRequest(request.result!));
       }).toJS;
       request.onblocked = ((Event e) {
         if (onBlocked != null) {
@@ -95,7 +138,7 @@ extension type idbFactory._(IDBFactory factory) {
 /// Transaction
 ///
 extension type Transaction._(IDBTransaction transaction) {
-  Transaction.fromDatabase(this.transaction);
+  Transaction._fromDatabase(this.transaction);
 }
 
 ///
@@ -111,7 +154,7 @@ extension type ObjectStore._(IDBObjectStore store) {
 /// An indexed database object for storing client-side data in web apps.
 ///
 extension type Database._(IDBDatabase database) {
-  Database.fromOpenRequest(JSAny result) : database = (result as IDBDatabase) {
+  Database._fromOpenRequest(JSAny result) : database = (result as IDBDatabase) {
     database.onabort = onAbortHandler();
     database.onclose = onCloseHandler();
     database.onerror = onErrorHandler();
@@ -216,7 +259,7 @@ extension type Database._(IDBDatabase database) {
       throw ArgumentError(mode);
     }
     final transaction = database.transaction(storeName_OR_storeNames, mode);
-    return Transaction.fromDatabase(transaction);
+    return Transaction._fromDatabase(transaction);
   }
 
   Transaction transactionList(List<String> storeNames, String mode) {
@@ -224,7 +267,7 @@ extension type Database._(IDBDatabase database) {
       throw ArgumentError(mode);
     }
     final transaction = database.transaction(storeNames.toJSBox, mode);
-    return Transaction.fromDatabase(transaction);
+    return Transaction._fromDatabase(transaction);
   }
 
   Transaction transactionStore(String storeName, String mode) =>
